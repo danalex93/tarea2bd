edd37a4ac07a5e1f339db2ccc32ed5898ff2f2f4
Daniel Alejandro Morales Miguel <daniel@devwith.us>
2014-06-01 16:50:21 -0400

    Login/Logout funcionando

--- Phoro/Controllers/UsuarioController.cs
+++ Phoro/Controllers/UsuarioController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Phoro.Models;
+using System.Collections.Specialized;
 
 namespace Phoro.Controllers
 {
@@ -117,6 +118,81 @@ namespace Phoro.Controllers
             db.Usuarios.Remove(usuario);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET: Usuario/Login
+        public ActionResult Login()
+        {
+            if (Request.Cookies["UserSettings"] != null)
+            {
+                return RedirectToAction("Index", "Categoria");
+            }
+            return View();
+        }
+
+        [HttpPost, ActionName("Login")]
+        public ActionResult Login(int? id)
+        {
+            NameValueCollection postData = Request.Form;
+            string user, pass;
+            if (!string.IsNullOrEmpty(postData["user"]))
+            {
+                user = postData["user"];
+            }
+            else
+                user = "";
+
+            if (!string.IsNullOrEmpty(postData["pass"]))
+            {
+                pass = postData["pass"];
+            }
+            else
+                pass = "";
+
+            //Process login
+            System.Diagnostics.Debug.Write("user: " + user + "pass: " + pass);
+            int loginResult = loginAction(user,pass);
+            if (loginResult != -1)
+            {
+                var User = db.Usuarios.Find(loginResult);
+                HttpCookie cookie = new HttpCookie("UserSettings");
+                cookie["Nombre"] = user;
+                cookie["Grupo"] = User.Grupo.id_grupo.ToString();
+                cookie["Id"] = User.id_usuario.ToString();
+                cookie.Expires = DateTime.Now.AddDays(1d);
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index", "Categoria");
+            }
+            else
+            {
+                ViewBag.Error = "Usuario/Contraseña erroneo";
+                return View();
+            }
+        }
+
+        private int loginAction(string user, string pass)
+        {
+            var temp = db.Usuarios
+                         .Where(x => x.nombre == user)
+                         .First();
+            Usuario User = db.Usuarios.Find(temp.id_usuario);
+            if (User.contrasena == pass)
+                return User.id_usuario;
+            else
+                return -1;
+
+        }
+
+        // GET: Usuario/Logout
+        public ActionResult Logout()
+        {
+            if (Request.Cookies["UserSettings"] != null)
+            {
+                HttpCookie cookie = new HttpCookie("UserSettings");
+                cookie.Expires = DateTime.Now.AddDays(-1d);
+                Response.Cookies.Add(cookie);
+            }
+            return RedirectToAction("Index", "Home");
         }
 
         protected override void Dispose(bool disposing)
